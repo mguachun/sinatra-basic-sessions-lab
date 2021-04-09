04490edcf0e27215c3f9ad93cad00604c7ccb084
Melissa Guachun <1212malka@gmail.com>
2021-04-08 21:57:06 -0400

    commit tree f6ed6a54677fc52904c99053f31434c577c051c5

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do 
+        enable :sessions
+        set :session_secret, 'flatiron'
+    end
+
+    get "/" do
+        erb :index
+    end
+
+    post '/checkout' do
+        session[:item] = params[:item]
+        @sessions = session_secret 
+        erb :checkout 
+    end
+
+
+
+
+
 end
